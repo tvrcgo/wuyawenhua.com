a7b8b9b3e1538ca4748bbd95a85e5064c3f851f6
tvrcgo <tvrcgo@gmail.com>
2020-11-04 17:50:19 +0800

    beian link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function () {
     <div className={styles.page}>
       <div><img className={styles.logo} src={require('@/assets/wuya.jpg')} /></div>
       <h1>乌鸦文化</h1>
-      <div className={styles.foot}>粤ICP备 2020110649号</div>
+      <div className={styles.foot}><a href='http://beian.miit.gov.cn' target='_blank'>粤ICP备2020110649号</a></div>
     </div>
   )
 }
